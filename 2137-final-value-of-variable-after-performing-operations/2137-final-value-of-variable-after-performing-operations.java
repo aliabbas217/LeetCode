c78cf61c6ad9b5c8379f6db320287cb6d5604279
Ali Abbas Baig <119006262+aliabbas217@users.noreply.github.com>
2023-09-30 22:29:05 +0500

    Time: 1 ms (72.5%), Space: 41.6 MB (30.6%) - LeetHub

--- 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.java
@@ -3,7 +3,7 @@ class Solution {
         byte x = 0;
         for(String operation: operations)
         {
-            if(operation.contains("++"))
+            if(operation.contains("+"))
             {
                 x+=1;
             }
