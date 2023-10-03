d17280f44273f5f69ed59ad8f9e841a6922f1392
Ali Abbas Baig <119006262+aliabbas217@users.noreply.github.com>
2023-10-04 00:27:36 +0500

    Time: 0 ms (100%), Space: 42.7 MB (88.36%) - LeetHub

---
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int arraySign(int[] nums)
+    {
+        short negatives = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] == 0)
+                return 0;
+            if(nums[i] < 0)
+                negatives++;
+        }
+        if(negatives%2 == 0)
+            return 1;
+        return -1;
+    }
+}
