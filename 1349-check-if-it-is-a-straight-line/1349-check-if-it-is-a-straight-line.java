731b0c428b7c55c031395d693fe8d2406599c62f
Ali Abbas Baig <119006262+aliabbas217@users.noreply.github.com>
2023-10-07 19:25:11 +0500

    Time: 0 ms (100%), Space: 43.4 MB (12.54%) - LeetHub

---
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkStraightLine(int[][] c) { 
+        // for(int i = 2; i < c.length; i++)
+        // {
+        //     if(c[i-1][1]*c[i-2][0]*(c[i-2][0] - c[i][0]) != ((c[i-1][0]*c[i-2][1]*(c[i-2][0]-c[i][0]))+((c[i-2][0]*c[i][1]-c[i][0]*c[i-2][1])*(c[i-2][0]-c[i-1][0]))))
+        //         return false;
+        // }
+        // return true;
+        for(int i = 2; i< c.length; i++)
+        {
+            if(
+                c[i][1] * (c[0][0]-c[1][0]) !=
+                ((c[0][1] - c[1][1])*c[i][0]) + (c[0][0]*c[1][1] - c[1][0]*c[0][1])
+            )
+            return false;
+        }
+        return true;
+    }
+}
