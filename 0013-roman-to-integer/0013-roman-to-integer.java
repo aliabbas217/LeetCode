d89fd68ab111de31db986b9cf226a2227a91cbb0
Ali Abbas Baig <119006262+aliabbas217@users.noreply.github.com>
2023-10-09 04:12:47 +0500

    Time: 2 ms (100%), Space: 43.6 MB (66.27%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -9,49 +9,42 @@ class Solution {
                 case 'I':
                     value+=1;
                     prev = 'I';
-                    System.out.println("at " + s.charAt(i) + " value: " + value + " prev: " + prev);
                     break;
                 case 'V':
                     value+=5;
                     if(prev == 'I')
                         value -= 2;
                     prev = 'V';
-                    System.out.println("at " + s.charAt(i) + " value: " + value + " prev: " + prev);
                     break;
                 case 'X':
                     value+=10;
                     if(prev == 'I')
                         value -= 2;
                     prev = 'X';
-                    System.out.println("at " + s.charAt(i) + " value: " + value + " prev: " + prev);
                     break;
                 case 'L':
                     value+=50;
                     if(prev == 'X')
                         value -= 20;
                     prev = 'L';
-                    System.out.println("at " + s.charAt(i) + " value: " + value + " prev: " + prev);
                     break;
                 case 'C':
                     value+=100;
                     if(prev == 'X')
                         value -= 20;
                     prev = 'C';
-                    System.out.println("at " + s.charAt(i) + " value: " + value + " prev: " + prev);
                     break;
                 case 'D':
                     value+=500;
                     if(prev == 'C')
                         value -= 200;
                     prev = 'D';
-                    System.out.println("at " + s.charAt(i) + " value: " + value + " prev: " + prev);
                     break;
                 case 'M':
                     value+=1000;
                     if(prev == 'C')
                         value -= 200;
                     prev = 'M';
-                    System.out.println("at " + s.charAt(i) + " value: " + value + " prev: " + prev);
                     break;
             }
         }
